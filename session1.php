9bb1f24a94fe0828799b005649fb5dc6ed008ec2
RadwaHelmy <90476756+RadwaHelmy@users.noreply.github.com>
2024-05-13 22:10:30 +0300

    session1.php

--- session1.php
+++ session1.php
@@ -2,5 +2,5 @@
 $first_school="future is the our target";
 $second_school="Happy kid in our happy family";
 echo "<h1>$first_school</h1> ,
-    <h2>$second_school<h2>" ;
+    <h2>$second_school</h2>" ;
 ?>
